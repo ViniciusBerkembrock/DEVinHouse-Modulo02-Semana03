54fae2102db98cf6e5b7c78218e1548db77526c3
ViniciusBerkembrock <113476905+ViniciusBerkembrock@users.noreply.github.com>
2023-02-06 19:37:31 -0300

    Adiciona correção do último exercício

--- Exercício 08/script.js
+++ Exercício 08/script.js
@@ -2,9 +2,11 @@ let mensagem = document.getElementById("mensamgem")
 let show = document.getElementById("show")
 let showLS = document.getElementById("showLocalStorage")
 let indexAdd = 0
+let teste = 1
 
 function salvarMensagem() {
-    localStorage.setItem(`${indexMensagemSalva()}`, mensagem.value)
+    localStorage.setItem(`${teste}`, mensagem.value)
+    teste++
 }
 
 function indexMensagemSalva(){
@@ -22,16 +24,32 @@ function adicionarMensagem(){
     show.innerHTML += `Mensagem Nº ${indexMensagemAdicionada()} ${mensagem.value}<br>`
 }
 
-function mostrarMensagem() {
-    let arr =[]
-    for (var i = 1; i <= localStorage.length; i++) {
-        // showLS += `Mensagem Nº ${localStorage.key(element)} ${localStorage.getItem(element+1)}<br>`
-        // arr.push(i, "xx")
-        // // console.log(localStorage.key(i))
-        // // console.log(localStorage.getItem(i))
-        // console.log(arr)
-        var key = localStorage.key(i)
-        console.log(key + localStorage.getItem(key))
-    }
+
+
+
+
+
+var arr = []
+
+function carregarMensagens(Element) {
     
+    arr[Element] = localStorage.getItem(Element)
+
+}
+
+
+
+function mostrarMensagem(){
+
+    Object.keys(localStorage).forEach(Element =>
+        
+        carregarMensagens(Element)
+    )
+    
+    arr.sort
+
+    for (index = 1; index < arr.length; index++){
+        showLS.innerHTML += `Mensagem Nº ${index}: ${arr[index]}<br>`
+    }
+
 }
